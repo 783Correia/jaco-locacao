e74bc4218c54c456eae552cf66800564806baaea
MacBook PRO <onetech@MacBook-Pro.local>
2026-03-03 17:19:01 -0300

    feat: adicionando tags GTM e GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,32 @@ export default async function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K9G8W3CJ');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z3CDLTLXVP" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-Z3CDLTLXVP');
+            `,
+          }}
+        />
         <Script
           id="schema-local-business"
           type="application/ld+json"
@@ -89,6 +115,16 @@ export default async function RootLayout({
         />
       </head>
       <body className={`${dmSans.variable} font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K9G8W3CJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SmoothScroll>
           {!isLP && <Header />}
           <main>{children}</main>
